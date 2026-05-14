4405df1eb9af1aa0e93749788db2285937414db1
Ego_Code <criss.2004.guaman@gmail.com>
2026-05-14 10:38:09 -0500

    subida de proyect0

---
+++ src/main/java/unl/edu/cc/cola/PeticionEnergia.java
@@ -0,0 +1,13 @@
+package unl.edu.cc.cola;
+
+public class PeticionEnergia {
+    String idDispositivo;
+    double kwhRequeridos;
+    long tiempoEntrada;
+
+    public PeticionEnergia(String id, double kwh) {
+        this.idDispositivo = id;
+        this.kwhRequeridos = kwh;
+        this.tiempoEntrada = System.currentTimeMillis();
+    }
+}
